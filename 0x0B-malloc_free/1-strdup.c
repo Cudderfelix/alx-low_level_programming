05bded6e19e66f18437ea0cbce1bf8bb53ac1af4
Cudderfelix <ifeachochukafelix@gmail.com>
2023-01-10 20:19:09 +0000

    Malloc Programming Sequence

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,35 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
+ * *_strdup - a function that duplicates a string
  *
- * @str: string to duplicate
- * Return: char pointer
- */
+ * @str: input string to duplicate
+ *
+ * Return: NULL if str == NULL
+ *         @str
+*/
+
 char *_strdup(char *str)
 {
-	char *sd;
-	unsigned int size = 0, i;
+	int i = 0, l = 0;
+	char *s;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		size++;
-	sd = malloc(sizeof(char) * (size + 1));
-	if (sd == NULL)
+
+	/*calculate size of str*/
+	while (str[l] != '\0')
+		l++;
+
+	s = malloc((l + 1) * sizeof(char));
+	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		sd[i] = str[i];
-	sd[size] = '\0';
-	return (sd);
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
 }
