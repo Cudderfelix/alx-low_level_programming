0e623fd0586366936615e5acba6142082a75189b
Cudderfelix <ifeachochukafelix@gmail.com>
2023-10-12 01:11:35 +0000

    C Pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+/**
+ * op_add - adds @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - gets the difference of @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
