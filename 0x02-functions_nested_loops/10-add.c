a7d0af0ab985a1193f5808b3d63377afdee311fc
Cudderfelix <ifeachochukafelix@gmail.com>
2023-11-09 12:42:31 +0100

    nestedLoops_add_two_integersv1.2

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add -add two integers
+ * 
+ * @d: first input
+ * @f: second input
+ * 
+ * Return: sum of a & b
+ */
+
+int add(int d, int f)
+{
+	int sum = d+f;
+
+	return (sum);
+}
