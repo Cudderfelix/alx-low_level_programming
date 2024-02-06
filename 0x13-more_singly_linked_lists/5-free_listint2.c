a86d359355d23de6aee93aa9247ada14e0566635
Cudderfelix <ifeachochukafelix@gmail.com>
2024-02-06 17:35:27 +0100

    moresinglylinkedlistsv1.3.15

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,4 @@
-v#include "lists.h"
+#include "lists.h"
 
 /**
  * free_listint2 - a function that frees the list and sets head to NULL
@@ -15,5 +15,14 @@ void free_listint2(listint_t **head)
 	{
 		/*set head addr to current*/
 		current = *head;
+		/*Traverse through the whole list*/
+		/*while setting the node to temp*/
+	while ((temp = current) != NULL)
+	{
+		/*set next node to current*/
+		current = current->next;
+		/*free temp, that is the current node*/
+		free(temp);
 	}
+	*head = NULL;
 }
