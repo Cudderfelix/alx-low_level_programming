7e0edfe8da2b4ccce9123af18bcc722e6a93161b
Cudderfelix <ifeachochukafelix@gmail.com>
2024-02-06 17:53:28 +0100

    moresinglylinkedlistsv1.3.18

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,3 +8,28 @@ hhd#include "lists.h"
  * Return: the head's node's data (n)
 */
 int pop_listint(listint_t **head)
+{
+	int first_node;
+	listint_t *temp, *next;
+
+	/*if list is empty return 0*/
+	if (*head == NULL)
+		return (0);
+	/*assign head addr to temp*/
+	temp = *head;
+
+	/*get addr of next node*/
+	next = temp->next;
+
+	/*get element of first node*/
+	first_node = temp->n;
+
+	/*free first node*/
+	free(temp);
+
+	/*set head to second node*/
+	*head = next;
+
+	/*return element of first node*/
+	return (first_node);
+}
