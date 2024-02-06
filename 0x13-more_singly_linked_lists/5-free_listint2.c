813315fcce1845efb5d39e9723b4312a8f87f53f
Cudderfelix <ifeachochukafelix@gmail.com>
2024-02-06 17:29:35 +0100

    moresinglylinkedlistsv1.3.14

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,17 +15,5 @@ void free_listint2(listint_t **head)
 	{
 		/*set head addr to current*/
 		current = *head;
-
-		/*iterate through the whole list*/
-		/*while setting the current node to temp*/
-		while ((temp = current) != NULL)
-		{
-			/*set next node to curretnt*/
-			current = current->next;
-			/*free temp, that is the current node*/
-			free(temp);
-		}
-
-		*head = NULL;
 	}
 }
