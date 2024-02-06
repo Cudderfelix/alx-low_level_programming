5c4a75b3d9fc93ba869c6ea40a35bf5a6048c650
Cudderfelix <ifeachochukafelix@gmail.com>
2024-02-06 18:17:34 +0100

    moresinglylinkedlistsv1.3.24

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -14,7 +14,10 @@ int sum_listint(listint_t *head)
 
 	while (head != NULL)
 	{
-	
+	/*add n to sum*/
+		sum += head->n;
+	/*go to the next node*/
+		head = head->next;
 	}
 
 	/*if list will be empty it will return sum = 0*/
