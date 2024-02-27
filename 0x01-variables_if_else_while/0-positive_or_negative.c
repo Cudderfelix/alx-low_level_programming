5b21c5b09f462b4ca522734bc775b83eeb80f112
Cudderfelix <ifeachochukafelix@gmail.com>
2024-02-27 12:55:43 +0100

    if_else_v1.3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: print value of n status; which should contain zero, positive or negative
+ * Description: print value of n status;
+ *  which should contain zero, positive or negative.
  *
  * Return:: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
